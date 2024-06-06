fdb6a2c9ce832495bab00deeb9e952b78d1b310c
spammenotinoz <spammenotinoz@gmail.com>
2024-06-06 16:01:58 +1000

    Update

--- web/src/components/settings.vue
+++ web/src/components/settings.vue
@@ -36,23 +36,23 @@ export default {
         <div class="mb-4 flex flex-col gap-2">
           <a href="https://smart.ultimateai.org" target="_blank">
             <Icon icon="mdi:chat-outline" width="24" height="24" />
-			<span>Smart Chat with Stable Diffusion</span>
+			<span> Smart Chat with Stable Diffusion</span>
           </a>
           <a href="https://new.ultimateai.org" target="_blank">
             <Icon icon="mdi:chat" width="24" height="24" />
-			<span>Ultimate Chat</span>
+			<span> Ultimate Chat</span>
           </a>
           <a href="https://gpt.ultimateai.org" target="_blank">
             <Icon icon="simple-icons:openai" width="24" height="24" />
-			<span>Chat GPT Plus</span>
+			<span> Chat GPT Plus</span>
           </a>
           <a href="https://umj.ultimateai.org/#/draw/1002" target="_blank">
             <Icon icon="logos:midjourney" width="24" height="24" />
-			<span>Midjourney</span>
+			<span> Midjourney</span>
           </a>
           <a href="https://umj.ultimateai.org/#/music/index" target="_blank">
             <Icon icon="mdi:music" width="24" height="24" />
-			<span>AI Music</span>
+			<span> AI Music</span>
           </a>
         </div>
         <div class="mb-4 flex flex-row gap-2">
