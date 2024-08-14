036e8ea9eeb846287fd171216c571cd1e269d7d0
spammenotinoz <spammenotinoz@gmail.com>
2024-08-14 12:12:01 +1000

    Update constant.ts

--- backend/constant.ts
+++ backend/constant.ts
@@ -24,7 +24,7 @@ export const Models: IModelInfo[] = [
   {
     platform: 'openai',
     type: 'openai',
-    models: ['gpt-3.5-turbo', 'gpt-4-0125-preview', 'gpt-4-turbo-preview', 'gpt-4o', 'gpt-4o-mini']
+    models: ['gpt-4o-mini']
   },
   {
     platform: 'baidu',
